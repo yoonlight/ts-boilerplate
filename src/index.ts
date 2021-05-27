b2aee5afbde631efdc4243b1d960488d824c700b
kjw <yoonlight@gmail.com>
2021-05-27 23:28:32 +0900

    apply gitignore

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
+import Koa from 'koa';
+const app = new Koa();
+
+app.use(async ctx => {
+  ctx.body = 'Hello World';
+});
+
+app.listen(process.env.PORT);
 let message: string = 'Hello World';
 console.log(message);
+console.log(process.env.PORT);
